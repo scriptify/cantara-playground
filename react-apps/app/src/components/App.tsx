a4562b0219c9b6f878c96c2da61e6865d607a4b4
scriptify <maximilian.torggler@tuta.io>
2022-03-19 07:53:37 +0100

    include jsqr

--- react-apps/app/src/components/App.tsx
+++ react-apps/app/src/components/App.tsx
@@ -1,12 +1,17 @@
-import React from "react";
-import { shiftJISTable } from "./shiftJISTable";
+import React, { useEffect } from 'react';
+import jsqr from 'jsqr';
+import { shiftJISTable } from './shiftJISTable';
 
 type Props = {};
 
 const App = (props: Props) => {
+  useEffect(() => {
+    jsqr(new Uint8ClampedArray(), 100, 100);
+  }, []);
+
   return (
     <div>
-      What a nice App!
+      What a nice App! Faaaaaakee
       <br />
       Shifted: {shiftJISTable[0x81a0]}
     </div>
