7357dbf519cf5a3daf285ff5ee43dff4f86a5c7f
scriptify <maximilian.torggler@tuta.io>
2022-04-02 15:09:29 +0200

    trigger deploy

--- react-apps/app/src/components/App.tsx
+++ react-apps/app/src/components/App.tsx
@@ -3,7 +3,7 @@ const QRCodeScanner = lazy(() => import('./QRCodeScanner'));
 
 type Props = {};
 
-console.log('trigger deploy 8');
+console.log('trigger deploy 9');
 
 const App = (props: Props) => {
   return (
