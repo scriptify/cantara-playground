1b6061915cd076e39622bbc0ab18e6315fced65b
scriptify <maximilian.torggler@tuta.io>
2022-07-15 09:22:21 +0200

    app with standard webpack setup for comparisoon

---
+++ standard-setup/src/components/App.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import "./../assets/scss/App.scss";
+import { shiftJISTable } from "./util";
+
+const reactLogo = require("./../assets/img/react_logo.svg");
+
+const App = () => {
+  console.log("Table ::::", shiftJISTable);
+
+  return (
+    <div className="app">
+      <h1>Hello World!</h1>
+      <p>Foo to the barz</p>
+      <img src={reactLogo.default} height="480" />
+    </div>
+  );
+};
+
+export default App;
